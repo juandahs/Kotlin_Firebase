ff17f242d712461f851d27c4c66d5ac8562e53b3
Juan David Castro Marin <jdcastro861@soy.sena.edu.co>
2024-11-28 08:55:23 -0500

    Se agrega la funcionalidad del scroll View y obtencion de pokemons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.glide)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/firebase/HomeActivity.kt
+++ app/src/main/java/com/example/firebase/HomeActivity.kt
@@ -1,14 +1,70 @@
 package com.example.firebase
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.firebase.databinding.ItemPokemonBinding
+import org.json.JSONException
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_home)
+        fetchPokemons()
+    }
 
+    private fun fetchPokemons() {
+        val url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                try {
+                    val dataArray = response.getJSONArray("results")
+                    val pokemonContainer = findViewById<LinearLayout>(R.id.llPokemonContainer)
+                    for (i in 0 until dataArray.length()) {
+                        val jsonObj = dataArray.getJSONObject(i)
+
+                        val name = jsonObj.getString("name")
+                        val url = jsonObj.getString("url")
+                        val pokemonId = url.split("/").filter { it.isNotEmpty() }.last()
+                        val userView = createPokemonView(pokemonId, name, url)
+                        pokemonContainer.addView(userView)
+                    }
+                } catch (e: JSONException) {
+                    Toast.makeText(this, "Error al procesar datos", Toast.LENGTH_SHORT).show()
+                }
+            },
+            {
+                Toast.makeText(this, "Error en la solicitud: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
+
+
+    private fun createPokemonView(id:String, name: String, url:String): LinearLayout {
+
+        val binding = ItemPokemonBinding.inflate(LayoutInflater.from(this))
+        binding.tvId.text = "ID: $id"
+        binding.tvName.text = "Name: $name"
+        binding.tvUrl.text = "URL: $url"
+
+        Glide.with(this)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png")
+            .centerCrop()
+            .circleCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(binding.ivAvatar)
+        return binding.root as LinearLayout
     }
 }
